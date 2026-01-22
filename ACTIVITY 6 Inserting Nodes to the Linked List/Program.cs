3bfc73eb3c9def61b375edfba3727fe1c0b2882b
rodrigodexter73-svg <rodrigodexter73@gmail.com>
2026-01-22 14:14:12 +0800

    Add files via upload

---
+++ ACTIVITY 6 Inserting Nodes to the Linked List/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTIVITY_6_Inserting_Nodes_to_the_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Dexter V. Rodrigo      //Date: Jan,22,2026      
+            //Section: IT402P
+            //Activity: Inserting Nodes to the Linked List (Procedural Approach)
+
+            LinkedList<string> list = new LinkedList<string>();
+
+            int count;
+
+            do
+            {
+                Console.Write("Enter number of names: ");
+                count = Convert.ToInt32(Console.ReadLine());
+            }
+            while (count < 5 || count > 10);
+
+            Console.WriteLine("\n(~~~My Friends Last Name~~~)");
+
+            //insert nodes using loop 
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Enter Friend LastName: ");
+                string name = Console.ReadLine();
+                list.AddLast(name); //node insert
+            }
+            //linked list display 
+            Console.WriteLine("\n(~~~Linked List Content~~~)");
+            foreach(string name in list)
+            {
+              Console.Write("Friends Last Names: ");
+              Console.WriteLine(name);
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ACTIVITY 6 Inserting Nodes to the Linked List/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACTIVITY 6 Inserting Nodes to the Linked List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACTIVITY 6 Inserting Nodes to the Linked List")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a59a332c-32a3-4c86-8884-4569680c0d81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
